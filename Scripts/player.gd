af27ee2887a0febaeeacbf68cfb0a358e56954b4
Likvik89 <erik@elfisk.dk>
2024-08-17 12:41:09 +0200

    Schmovement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,12 +3,12 @@ extends RigidBody2D
 var speed = 0
 const acceleration = 40
 const deacceleration = 10
-var stopping_power = 0.9
+var stopping_power = 0.8
 const maxspeed = 300
 var dashing = false
 var X = 0
 var Y = 0
-var current_direction = Vector2(0,0)
+
 
 
 func _physics_process(delta):
@@ -36,25 +36,29 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_up"):
 		#linear_velocity += Vector2(0,-1)*speed
 		if Y > -1:
-			Y -= 0.15
+			Y -= 0.2
 		
-		current_direction = linear_velocity
 	elif Input.is_action_pressed("move_down"):
 		#linear_velocity += Vector2(0,1)*speed
 		if Y < 1:
-			Y += 0.15
-		current_direction = linear_velocity
-	else: Y = 0
+			Y += 0.2
+	else: 
+		if Y < 0:
+			Y += 0.2
+		if Y > 0:
+			Y -= 0.2
 	if Input.is_action_pressed("move_left"):
 		#linear_velocity += Vector2(-1,0)*speed
 		if X > -1:
-			X -= 0.15
-		current_direction = linear_velocity
+			X -= 0.2
 	elif Input.is_action_pressed("move_right"):
 		#linear_velocity += Vector2(1,0)*speed
 		if X < 1:
-			X += 0.15
-		current_direction = linear_velocity
-	else: X = 0
+			X += 0.2
+	else: 
+		if X < 0:
+			X += 0.2
+		if X > 0:
+			X -= 0.2
 	
 
